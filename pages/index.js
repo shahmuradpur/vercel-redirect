e4e96fb49876af773c3d6ac8ffcb540f2cb9f745
shahmuradpur <amzteam9@gmail.com>
2025-01-12 12:43:40 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Vercel Redirect Project</h1>
+            <p>Share your links with rich metadata previews.</p>
+        </div>
+    );
+}
